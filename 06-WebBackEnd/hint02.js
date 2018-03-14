b5c755e649afd3a5395c0cc8230a88d6b631d766
Katie Arriaga M <katiearriaga@live.com>
2018-03-14 17:09:28 -0600

    Create hint02.js

---
+++ 06-WebBackEnd/hint02.js
@@ -0,0 +1,14 @@
+function postComment(){
+	console.log("clicked post comment");
+
+	if($("#commentField").val() !== null && $("#commentField").val() !=""){
+		var stringToPrepend = '<li>' + 
+				'<blockquote>' + 
+					'<div class="commentContent">' + 
+						$("#commentField").val() + 
+					'</div>' + 
+				'</blockquote>' + 
+			'</li>';
+		$("#commentField").val("");
+	}
+}
