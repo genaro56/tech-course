cd659ccb024fb0452ab32f098f1651b3f0ae4632
Katie Arriaga M <katiearriaga@live.com>
2018-03-14 19:04:26 -0600

    Update hint02.js

--- 06-WebBackEnd/hint02.js
+++ 06-WebBackEnd/hint02.js
@@ -1,15 +1,21 @@
+function formatCommentString(comment){
+	var stringToPrepend = '<li>' + 
+				'<blockquote>' + 
+					'<div class="commentContent">' + 
+						comment + 
+					'</div>' + 
+				'</blockquote>' + 
+			'</li>';
+	return stringToPrepend;
+}
+
 function postComment(){
 	console.log("clicked post comment");
 
 	if($("#commentField").val() !== null && $("#commentField").val() !=""){
-		var stringToPrepend = '<li>' + 
-				'<blockquote>' + 
-					'<div class="commentContent">' + 
-						$("#commentField").val() + 
-					'</div>' + 
-				'</blockquote>' + 
-			'</li>';
-		$("#commentList").prepend(stringToPrepend);
+
+		var stringToPrepend = formatCommentString($("#commentField").val());
+		AddCommentToDB($("#commentField").val(), stringToPrepend);
 		$("#commentField").val("");
 	}
 }
